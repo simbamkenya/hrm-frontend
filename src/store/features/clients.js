0973404b24030b9368360438da8c1d355f9142ed
simbankenya <pharesmuruthi@gmail.com>
2023-06-08 16:27:28 +0300

    handled client delete

--- src/store/features/clients.js
+++ src/store/features/clients.js
@@ -1,27 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const clientSlice = createSlice({
-    name: 'clients',
-    initialState: {
-        clients: []
+  name: 'clients',
+  initialState: {
+    clients: [],
+  },
+  reducers: {
+    clientsReceived: (state, action) => {
+      console.log('clients receivved')
+      state.clients = action.payload
     },
-    reducers: {
-        clientsReceived : (state, action) =>{
-            console.log('clients receivved')
-            state.clients = action.payload;
-        },
-        add: (state, action) => {
-            state.clients.push({
-                clientName: action.payload.clientName,
-                email: action.payload.email,
-                address: action.payload.address,
-            })
-        },
-        
-    }
-});
+    add: (state, action) => {
+      state.clients.push({
+        clientName: action.payload.clientName,
+        email: action.payload.email,
+        address: action.payload.address,
+      })
+    },
+    deleteClient: (state, action) => {
+      state.clients = state.clients.filter(
+        (client) => client._id !== action.payload._id
+      )
+    },
+  },
+})
 
-
-const { actions, reducer } = clientSlice;
-export const  { add, projectsReceived } = actions;
-export default reducer;
+const { actions, reducer } = clientSlice
+export const { add, projectsReceived } = actions
+export default reducer
