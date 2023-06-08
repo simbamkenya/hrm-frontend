72a938325d04b828526f6b67ec59ba44ab64149a
simbankenya <pharesmuruthi@gmail.com>
2023-06-08 16:43:54 +0300

    handled project delete

--- src/store/features/projects.js
+++ src/store/features/projects.js
@@ -1,26 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const projectSlice = createSlice({
-    name: 'projects',
-    initialState: {
-        projects: []
+  name: 'projects',
+  initialState: {
+    projects: [],
+  },
+  reducers: {
+    projectsReceived: (state, action) => {
+      console.log('project reached')
+      state.projects = action.payload
     },
-    reducers: {
-        projectsReceived: (state, action) => {
-            console.log('project reached')
-            state.projects = action.payload;
-        },
-        add: (state, action) => {
-            state.projects.push({
-                projectName: action.payload.projectName,
-                hours: action.payload.hours,
-                dateDue: action.payload.dateDue,
-                client: action.payload.client,
-            })
-        }
-    }
-});
+    add: (state, action) => {
+      state.projects.push({
+        projectName: action.payload.projectName,
+        hours: action.payload.hours,
+        dateDue: action.payload.dateDue,
+        client: action.payload.client,
+      })
+    },
+    deleteProject: (state, action) => {
+      state.projects = state.projects.filter(
+        (project) => project._id !== action.payload._id
+      )
+    },
+  },
+})
 
-const { actions, reducer } = projectSlice;
-export const { add, projectsReceived } = actions;
-export default reducer;
+const { actions, reducer } = projectSlice
+export const { add, projectsReceived, deleteProject } = actions
+export default reducer
